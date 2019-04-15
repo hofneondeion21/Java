35dc281b630a212c7f9cceac0fc1d89c9e22ef2e
Lu Sz <lszydlowski_5888@email.ric.edu>
2019-04-15 18:04:39 -0400

    updated

--- EclipseWork/JavaEarlyObjectsBook/src/ClassesAndObjects/PetTester.java
+++ EclipseWork/JavaEarlyObjectsBook/src/ClassesAndObjects/PetTester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PetTester {
 
 	private static Pet p = new Pet();
-	private static Pet pet = new Pet();
+//	private static Pet pet = new Pet();
 	public static void main(String[] args) {
 		Scanner z = new Scanner(System.in);
 		System.out.print("Enter Pet Name:\nEnter Pet Type:\nEnter Pet Age:\n");
