a65e15125f036e51d09b911247e670245f6eaab4
Lu Sz <lszydlowski_5888@email.ric.edu>
2019-05-01 14:19:41 -0400

    updated

--- EclipseWork/GUI/src/Main.java
+++ EclipseWork/GUI/src/Main.java
@@ -1,34 +1,12 @@
 import java.util.Scanner;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
 	private static Scanner z = new Scanner(System.in);
 	public static void main(String args[]) {
 		
-		System.out.println("WITHOUT SKIP!");
-		withOutSkip();
-		System.out.println("\n\nWITH SKIP!\n");
-		withSkip();
-
-		
-		
+	
 	}
 	
-	
-	
-	public static void withOutSkip() {
-		int a = z.nextInt();
-		System.out.println(" Int: "+a);
-//		z.skip(" \n");
-		String b = z.nextLine();
-		System.out.println(" String: "+b);
-	}
-	
-	
-	public static void withSkip() {
-		int a = z.nextInt();
-		System.out.println(" Int: "+a);
-		z.skip("\n");
-		String b = z.nextLine();
-		System.out.println(" String: "+b);
-	}
 }
